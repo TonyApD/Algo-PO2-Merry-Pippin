c6c870448adc1d25e940cb5563656f62eb7d5c10
TonyApD <t.lopar@student.han.nl>
2017-12-18 20:19:35 +0100

    Testcases reg

--- src/test/java/MoneySaveAppTest.java
+++ src/test/java/MoneySaveAppTest.java
@@ -9,12 +9,12 @@ import static org.junit.Assert.assertEquals;
 
 public class MoneySaveAppTest {
 
-    private MoneySaverApp app;
+    private SubstractMaximum app;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Before
     public void setUp() {
-        app = new MoneySaverApp();
+        app = new SubstractMaximum();
         System.setOut(new PrintStream(outContent));
     }
 
@@ -52,10 +52,22 @@ public class MoneySaveAppTest {
                 "9 4 4 7 11 3 13", 40);
     }
 
+    @Test
+    public void testExample8() {
+        executeAlgorithm("60 2\n" +
+                "5 3 6 8 9 5 4 3 2 5 5 3 6 8 9 5 4 3 2 5 5 3 6 8 9 5 4 3 2 5 5 3 6 8 9 5 4 3 2 5 5 3 6 8 9 5 4 3 2 5 5 3 6 8 9 5 4 3 2 5", 290);
+    }
+
+    @Test
+    public void testExample9(){
+        executeAlgorithm("5 3\n" +
+                "6 5 4 6 5", 20);
+    }
+
     private void executeAlgorithm(String input, int expectedOutput) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
 
-        MoneySaverApp.main(null);
+        SubstractMaximum.main(null);
 
         assertEquals(String.valueOf(expectedOutput), outContent.toString());
     }
