f80da91c42ab3cbd229ce018fd6c6f7b20711230
TonyApD <t.lopar@student.han.nl>
2017-12-18 22:51:40 +0100

    Added max profit algo

---
+++ src/main/java/SubstractMaximum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SubstractMaximum {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+
+        int nrOfProducts = scan.nextInt();
+        int nrOfDividers = scan.nextInt();
+
+        for (int i = 0; i < nrOfProducts; i++) {
+            sum += scan.nextInt();
+        }
+
+        System.out.print(Util.round(sum - (nrOfDividers + 1) * 4));
+    }
+}
