dac5512689b98f731b988ad490935cc1ffaf9bbb
Carlo Jessurun <carlojessurun@ip-145-97-27-178.wlan-ext.ru.nl>
2017-12-19 13:23:56 +0100

    removed last few cases

--- src/main/java/ProfitBasedAlgorithm.java
+++ src/main/java/ProfitBasedAlgorithm.java
@@ -8,39 +8,17 @@ public class ProfitBasedAlgorithm {
         int sum = 0;
         int temp = 0;
         int maxPossibleProfit = (4 + (nrOfDividers * 4));
-        ArrayList<Integer> profitListBySum = new ArrayList<>();
-        ArrayList<Integer> profitListByNum = new ArrayList<>();
+
         ArrayList<Integer> profitListByUnknown = new ArrayList<>();
 
-
-        // By sum calculations
-        for (Integer i : input) {
-            int w;
-            temp += i;
-            w = +temp % 10;
-            if (w > 0 && w < 5) {
-                profitListBySum.add(w);
-            }
-        }
-
-        // By num calculations
-        for (Integer i : input) {
-            int n;
-            n = +i % 10;
-            if (n > 0 && n < 5) {
-                profitListByNum.add(n);
-            }
-        }
-
-        temp = 0;
         int w = 0;
         // By unknown algorithm
-        for (int i = 0; i < input.size(); i++) {
+        for (Integer i : input) {
 
             //temp += input.get(i);
-            w = w + (input.get(i) % 10);
+            w = w + (i % 10);
             if (!profitListByUnknown.isEmpty() &&
-                    w + profitListByUnknown.get(profitListByUnknown.size() - 1)  <= 4) {
+                    w + profitListByUnknown.get(profitListByUnknown.size() - 1) <= 4) {
                 profitListByUnknown.set(profitListByUnknown.size() - 1, w + profitListByUnknown.get(profitListByUnknown.size() - 1));
                 w = 0;
             } else if (w % 10 > 0 && w % 10 < 5) {
@@ -53,20 +31,11 @@ public class ProfitBasedAlgorithm {
         for (Integer i : input) {
             temp += i;
         }
-        // Choose which profitlist we will use
-        ArrayList<Integer> optimumList;
-        if (profitListByUnknown.stream().mapToInt(Integer::intValue).sum() <
-                profitListBySum.stream().mapToInt(Integer::intValue).sum()) {
-            optimumList = profitListBySum;
-        } else {
-            optimumList = profitListByUnknown;
-        }
-
 
         int totalSubtracted = 0;
         sum += temp;
-        Collections.sort(optimumList, Collections.reverseOrder());
-        for (Integer i : optimumList) {
+        profitListByUnknown.sort(Collections.reverseOrder());
+        for (Integer i : profitListByUnknown) {
             if (nrOfDividers != 0) {
                 if (totalSubtracted != maxPossibleProfit) {
                     sum -= i;
