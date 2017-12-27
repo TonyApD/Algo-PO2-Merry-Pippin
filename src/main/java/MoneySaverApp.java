fa3251f54a95826d4f3e2908e27b2b7b5e43cf44
TonyApD <t.lopar@student.han.nl>
2017-12-27 22:33:19 +0100

    Small refinements(3 cases fail)

--- src/main/java/MoneySaverApp.java
+++ src/main/java/MoneySaverApp.java
@@ -1,25 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class MoneySaverApp {
 
     public static void main(String[] args) {
         List<Integer> groups;
-        Scanner scan = new Scanner(System.in);
+        Reader s = new Reader();
+        List<Integer> prizes = new ArrayList<>();
+        int p = s.nextInt();
+        int n = s.nextInt();
 
-        int nrOfProducts = scan.nextInt();
-        int nrOfDividers = scan.nextInt();
-
-        int[] prizes = new int[nrOfProducts];
-
-        for (int i = 0; i < nrOfProducts; i++) {
-            prizes[i] = scan.nextInt();
+        for (int i = 0; i < p; i++) {
+            prizes.add(s.nextInt());
         }
+        s.close();
 
         groups = searchProfitableGroups(prizes);
 
-        while (groups.size() > nrOfDividers + 1) {
+        while (groups.size() > n + 1) {
             removeDivider(groups);
         }
 
@@ -32,26 +30,26 @@ public class MoneySaverApp {
      * @param prizes the array of prizes all products
      * @return the list with profitable groups
      */
-    private static List<Integer> searchProfitableGroups(int[] prizes) {
+    private static List<Integer> searchProfitableGroups(List<Integer> prizes) {
         List<Integer> groups = new ArrayList<>();
         int currentGroupCost = 0;
-        for (int i = 0; i < prizes.length; i++) {
-            if ((currentGroupCost + prizes[i]) % 10 == 4) {
-                groups.add(currentGroupCost + prizes[i]);
+        for (int i = 0; i < prizes.size(); i++) {
+            if ((currentGroupCost + prizes.get(i)) % 10 == 4) {
+                groups.add(currentGroupCost + prizes.get(i));
                 currentGroupCost = 0;
-            } else if ((currentGroupCost + prizes[i]) % 10 < 4) {
-                if (Util.getP(currentGroupCost + prizes[i]) >= Util.getP(currentGroupCost)) {
-                    currentGroupCost += prizes[i];
+            } else if ((currentGroupCost + prizes.get(i)) % 10 < 4) {
+                if (Util.getP(currentGroupCost + prizes.get(i)) >= Util.getP(currentGroupCost)) {
+                    currentGroupCost += prizes.get(i);
                 } else {
                     groups.add(currentGroupCost);
-                    currentGroupCost = prizes[i];
+                    currentGroupCost = prizes.get(i);
                 }
             } else {
                 if (currentGroupCost % 10 < 5 && currentGroupCost % 10 > 0) {
                     groups.add(currentGroupCost);
-                    currentGroupCost = prizes[i];
+                    currentGroupCost = prizes.get(i);
                 } else {
-                    currentGroupCost += prizes[i];
+                    currentGroupCost += prizes.get(i);
                 }
 
             }
@@ -72,18 +70,22 @@ public class MoneySaverApp {
      */
     private static void removeDivider(List<Integer> groups) {
         //First check whether two groups may be merged that the total sum of the groups remains equal
-        for (int i = 0; i < groups.size() - 1; i++) { //TODO: Traversing from end to begin let's c5 fail suddenly. Check this
+        for (int i = groups.size() - 2; i >= 0; i--) {
             if (Util.r(groups.get(i) + groups.get(i + 1)) == Util.r(groups.get(i)) + Util.r(groups.get(i + 1))) {
                 groups.set(i, groups.get(i) + groups.get(i + 1));
                 groups.remove(i + 1);
-                return; //TODO: Check why return lets some cases not fail
+                return; //Divider removed, so don't execute next loop
             }
         }
 
         //Otherwise check which divider may be removed with the least impact on the total sum
+        //Only combinations of 1-4, 2-3, 2-4, 3-3, 3-4 and 4-4 may appear since the loop above combines all other combinations.
+        //In this order you want to also combine them if no consecutive combinations are possible anymore?
+        // Maybe try some pattern matching? For example: 6 * 4 = 24 is 4 profit. Think that something like this could be a fix.
+        // Or randomized check x elements and couple the pair of least impact. Not very consistent?
         int removeDividerAfter = 0;
         int currentLeastProfitLoss = (int) Double.MAX_VALUE;
-        for (int i = groups.size() - 2; i >= 0; i--) {
+        for (int i = 0; i < groups.size() - 1; i++) {
             if ((groups.get(i) % 10 + groups.get(i + 1) % 10) < currentLeastProfitLoss) {
                 removeDividerAfter = i;
                 currentLeastProfitLoss = groups.get(i) % 10 + groups.get(i + 1) % 10;
