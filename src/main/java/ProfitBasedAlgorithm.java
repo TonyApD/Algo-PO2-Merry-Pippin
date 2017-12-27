4469ab93cbd02f710d7d08bb2fd3919454c59fa1
Carlo Jessurun <carlojessurun@MacBook-Pro-van-Carlo.local>
2017-12-27 09:20:52 +0100

    Made the algo 2x faster

--- src/main/java/ProfitBasedAlgorithm.java
+++ src/main/java/ProfitBasedAlgorithm.java
@@ -4,25 +4,59 @@ import java.util.Scanner;
 
 public class ProfitBasedAlgorithm {
 
-    private static String inputTool(ArrayList<Integer> input, int nrOfDividers) {
-        int w = 0;
-        int sum = 0;
+    private static int sum;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        scan.nextInt();
+        int nrOfDividers = scan.nextInt();
+
+        ArrayList<Integer> input = new ArrayList<>();
+
+        while (scan.hasNext()) {
+            input.add(scan.nextInt());
+        }
+
+        System.out.print(Util.round(optimizeDividers(optimizePrices(input), nrOfDividers)));
+    }
+
+    /**
+     * Takes the main input with products and optimizes it into a possible profits which get stored into the profitList
+     *
+     * @param input main input for current list of products
+     * @return profitList with maximal amount that could be subtracted for that given input
+     */
+    private static ArrayList<Integer> optimizePrices(ArrayList<Integer> input) {
+        int p = 0; // p for profit, we use this to keep track of the profit at a given input
+        sum = 0;
         ArrayList<Integer> profitList = new ArrayList<>();
 
         for (Integer i : input) {
             sum += i;
-            w = w + (i % 10);
-            if (!profitList.isEmpty() && (w % 10) + profitList.get(profitList.size() - 1) <= 4) {
-                profitList.set(profitList.size() - 1, (w % 10) + profitList.get(profitList.size() - 1));
-                w = 0;
-            } else if (w % 10 > 0 && w % 10 < 5) {
-                profitList.add(w % 10);
-                w = 0;
+            p = p + (i % 10);
+            if (!profitList.isEmpty() && (p % 10) + profitList.get(profitList.size() - 1) <= 4) {
+                profitList.set(profitList.size() - 1, (p % 10) + profitList.get(profitList.size() - 1));
+                p = 0;
+            } else if (p % 10 > 0 && p % 10 < 5) {
+                profitList.add(p % 10);
+                p = 0;
             }
         }
+        return profitList;
+    }
 
-        profitList.sort(Collections.reverseOrder());
-        for (Integer i : profitList) {
+    /**
+     * Given that the profitlist is build up, we now look at placing dividers.
+     * Right now the list is sorted from high to low, then we subtract those top values as long as we have dividers
+     *
+     * @param priceOptimizedList list of possible subtractions based on the optimizePrices function
+     * @param nrOfDividers       needed to calculate the max sum to be subtracted.
+     * @return the return value consists of the optimized price that will get payed after 'placing' the dividers
+     */
+    private static int optimizeDividers(ArrayList<Integer> priceOptimizedList, int nrOfDividers) {
+        priceOptimizedList.sort(Collections.reverseOrder());
+        for (Integer i : priceOptimizedList) {
             if (nrOfDividers != 0) {
                 sum -= i;
                 nrOfDividers--;
@@ -30,20 +64,7 @@ public class ProfitBasedAlgorithm {
                 break;
             }
         }
-        return String.valueOf(Util.round(sum));
+        return sum;
     }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int nrOfProducts = scan.nextInt();
-        int nrOfDividers = scan.nextInt();
-
-        ArrayList<Integer> cost = new ArrayList<>();
-
-        for (int i = 0; i < nrOfProducts; i++) {
-            cost.add(i, scan.nextInt());
-        }
-        System.out.print(ProfitBasedAlgorithm.inputTool(cost, nrOfDividers));
-    }
 }
